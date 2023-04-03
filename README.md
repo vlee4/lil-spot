a66105ef42d81cd04168176eec3b3778623a3183
vlee4 <vicglee1234007@gmail.com>
2023-04-03 17:54:35 -0500

    Create minimal node-express backend to serve index

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lil-Spot
+
+A little Spotify application made during MLH's Global Hack Week: API's
+
+## Run Server
+
+In the terminal run, `npm run start`. The server will run on
+`http://localhost:3000/`.
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname+'/public/index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
